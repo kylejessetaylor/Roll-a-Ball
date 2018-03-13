41a70879e51e15587244e03408b599d874579ab3
Kyle Taylor <kyle.jesse.taylor@gmail.com>
2018-03-13 17:00:01 +1100

    Added:  - Lights
    Updated:
     - Combined Score.cs > Manager.cs
    
    Removed:
     - Score.cs

--- Tunnel Ball/Assets/Tunnel Ball/Scripts/Manager.cs
+++ Tunnel Ball/Assets/Tunnel Ball/Scripts/Manager.cs
@@ -6,50 +6,144 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
 
-	//Menu Buttons
-	public void PlayBtn (string startGame) {
-		//Loads a scene.
-		SceneManager.LoadScene (startGame);
-		Cursor.visible = false;
-	}
-
-	public void ExitGameBtn () {
-		//Exits Game.
-		Application.Quit ();
-	}
-
-	public void RestartGame (string restartGame) {
-		//Reloads scene.
-		SceneManager.LoadScene (restartGame);
-		Cursor.visible = false;
-	}
-
-	private void StartExitButtons () {
-		if (GameObject.FindGameObjectWithTag ("Player") == null) {
-			if (Input.GetKeyDown (KeyCode.Return)) {
-				SceneManager.LoadScene ("Tunnel");
-				Cursor.visible = false;
-			}
-			if (Input.GetKeyDown (KeyCode.Escape)) {
-				Application.Quit ();
-			}
-		}
-	}
-
-
-	public GameObject tunnelPrefab;
+    [Header("Game Setup")]
+	public GameObject firstTunnel;
 
 	void Start () {
-		BuildLevel (tunnelPrefab);
-	}
+        //Builds First Tunnel
+		BuildLevel (firstTunnel);
 
-	//Places Tunnel_001 on game start
-	private void BuildLevel (GameObject tunnelPieceToPlace) {
-		//GameObject newTunnel =
-            TrashMan.spawn (tunnelPieceToPlace, (Vector3.forward * -0.24f), Quaternion.identity);
-	}
+        //Loads in the current highscore into the game on launch
+        currentHighScore = PlayerPrefs.GetFloat("Highscore");
+        highScore.text = "Highscore: " + PlayerPrefs.GetFloat("Highscore");
+        stopScore = false;
+    }
 
 	void Update (){
 		StartExitButtons ();
-	}
+
+        //Updates Score
+        if (stopScore == false)
+        {
+            CurrentScore();
+        }
+    }
+
+    #region FirstTunnel+Buttons
+
+    //Places Tunnel_001 on game start
+    private void BuildLevel(GameObject tunnelPieceToPlace)
+    {
+        //GameObject newTunnel =
+        TrashMan.spawn(tunnelPieceToPlace, (Vector3.forward * -0.24f), Quaternion.identity);
+    }
+
+    //Menu Buttons
+    public void PlayBtn(string startGame)
+    {
+        //Loads a scene.
+        SceneManager.LoadScene(startGame);
+        Cursor.visible = false;
+    }
+
+    public void ExitGameBtn()
+    {
+        //Exits Game.
+        Application.Quit();
+    }
+
+    public void RestartGame(string restartGame)
+    {
+        //Reloads scene.
+        SceneManager.LoadScene(restartGame);
+        Cursor.visible = false;
+    }
+
+    private void StartExitButtons()
+    {
+        if (GameObject.FindGameObjectWithTag("Player") == null)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene("Tunnel");
+                Cursor.visible = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+    #endregion
+
+    #region Score
+
+    [Header("Score")]
+    public Text score;
+    public Text highScore;
+    public Text finalScore;
+    public Text whoops;
+
+    public float scoremultiplier = 0.2f;
+    protected float endScore;
+    protected float scorez;
+    protected float currentHighScore;
+
+    private bool stopScore = false;
+
+    public void CurrentScore()
+    {
+        //Score calculator
+        scorez = scoremultiplier * Mathf.Pow(Time.timeSinceLevelLoad, 1.5f);
+        score.text = "Score: " + Mathf.Round(scorez);
+
+        //Shows player's current score that round
+        if (scorez <= currentHighScore && (GameObject.FindGameObjectWithTag("Player") == null))
+        {
+            YourScore();
+        }
+        //Saves new highscore and applies text when player dies
+        if (scorez > currentHighScore && (GameObject.FindGameObjectWithTag("Player") == null))
+        {
+            SaveScore();
+            NewHighScore();
+        }
+    }
+
+    //Shows Score on Endgame screne
+    public void YourScore()
+    {
+        finalScore.text = "Your Score: " + Mathf.Round(scorez);
+        whoops.text = "Whoops!";
+
+        //textChange [Random.Range (0, textList.Count)]
+
+        //Stops numbers from continuing to calculate after death
+        stopScore = true;
+    }
+
+    //Updates and Shows Highscore on Endgame screne
+    public void NewHighScore()
+    {
+        finalScore.text = "Your Highscore: " + Mathf.Round(scorez);
+        whoops.text = "New Highscore!";
+
+        //Loads the new highscore into the highscore text
+        highScore.text = "Highscore: " + ((int)PlayerPrefs.GetFloat("Highscore")).ToString();
+
+        //Stops numbers from continuing to calculate after death
+        this.gameObject.GetComponent<Score>().enabled = false;
+    }
+
+    //Saves new Highscore
+    public void SaveScore()
+    {
+        PlayerPrefs.SetFloat("Highscore", Mathf.Round(scorez));
+    }
+
+    //	private void FunnyText (Text textChange) {
+    //
+    //	}
+
+    #endregion
 }
